95df9882b49fc71d1abbd09ff7b26373e3d6604e
tervicke <ameywpatil@gmail.com>
2025-03-21 18:52:31 +0530

    added src

---
+++ src/binance_producer.py
@@ -0,0 +1,58 @@
+from confluent_kafka import Producer
+import json
+import websocket
+
+config = {
+        "bootstrap.servers": "localhost:9092",
+}
+
+producer = Producer(config)
+
+def aftersend(err,send):
+    if not err == None:
+        print(f"error occured {err}")
+        return
+    print("Message sent")
+
+topic="test-topic"
+
+#producer.produce(topic, json_data ,callback=aftersend)
+
+last_updated_price = None; #string
+
+def on_message(ws, message):
+    global last_updated_price
+    data = json.loads(message)
+    price = data.get("p")  # Price is in string format
+    try:
+        if(price != last_updated_price):
+            print("Price changed!")
+            last_updated_price = price
+            producer.produce(topic , last_updated_price , callback=aftersend)
+            producer.flush()
+    except:
+        print(price)
+        print("error converting value")
+
+def on_error(ws, error):
+    print("Error:", error)
+
+def on_close(ws, close_status_code, close_msg):
+    print("WebSocket closed")
+
+def on_open(ws):
+    subscribe_message = {
+        "method": "SUBSCRIBE",
+        "params": ["solusdt@trade"],
+        "id": 1
+    }
+    ws.send(json.dumps(subscribe_message))
+
+
+
+
+if __name__ == "__main__":
+    ws_url = "wss://stream.binance.com:9443/ws/solusdt@trade"
+    ws = websocket.WebSocketApp(ws_url, on_message=on_message, on_error=on_error, on_close=on_close)
+    ws.on_open = on_open
+    ws.run_forever()
