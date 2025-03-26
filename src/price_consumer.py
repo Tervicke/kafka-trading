aa23d08b270d3b58f63b9b4a15104e709f806f9f
tervicke <ameywpatil@gmail.com>
2025-03-26 22:14:49 +0530

    added the calculate spread method

--- src/price_consumer.py
+++ src/price_consumer.py
@@ -1,24 +1,42 @@
 from confluent_kafka import Consumer, KafkaException
-
+import math
 conf  = {
         'bootstrap.servers':'localhost:9092',
         'group.id':"price consumer",
 }
+def calculate_spread(binance_price , coinbase_price ):
+    if(binance_price == -1 or coinbase_price == -1):
+        return
+    print("------------------------------------")
+    print(f"Binance Price {binance_price}")
+    print(f"Coinbase Price {coinbase_price}")
+    spread = abs(coinbase_price - binance_price)
+    print(f"Spread {spread}")
+    spread_percentage = (spread/max(coinbase_price , binance_price)) * 100
+    print(f"spread % {spread_percentage:.4f}%")
+    print("------------------------------------")
 
 consumer = Consumer(conf);
 consumer.subscribe(["usdt-sol-prices"])
+last_binance_price = -1
+last_coinbase_price = -1
 
 try:
     while True:
-        msg = consumer.poll(timeout=1.0)
-        if msg is None:
+        msg = consumer.poll(timeout=0)
+        if msg is None or msg.value is None:
             continue
         if msg.error():
             print(f"consumer error : {msg.error()}")
             continue;
         price_partition = msg.partition()
         price = float(msg.value().decode('utf-8'))
-        print(f"{price_partition} => {price}")
+        if(price_partition == 0):
+            last_coinbase_price = price
+        else:
+            last_binance_price = price
+        calculate_spread(last_binance_price , last_coinbase_price)
+
 except KeyboardInterrupt:
     print("Stopped")
 finally:
